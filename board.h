bcca6f49a9c92da4a03104fcc01ee053efef60f0
gcli7 <star106121@gmail.com>
2018-12-06 15:52:57 +0800

    edit board.h to 2x3

--- board.h
+++ board.h
@@ -5,20 +5,18 @@
 #include <cmath>
 
 /**
- * array-based board for 2048
+ * array-based board for Threes
  *
  * index (1-d form):
- *  (0)  (1)  (2)  (3)
- *  (4)  (5)  (6)  (7)
- *  (8)  (9) (10) (11)
- * (12) (13) (14) (15)
+ *  (0)  (1)  (2)
+ *  (3)  (4)  (5)
  *
  */
 class board {
 public:
 	typedef uint32_t cell;
-	typedef std::array<cell, 4> row;
-	typedef std::array<row, 4> grid;
+	typedef std::array<cell, 3> row;
+	typedef std::array<row, 2> grid;
 	typedef uint64_t data;
 	typedef int reward;
 
@@ -32,8 +30,8 @@ public:
 	operator const grid&() const { return tile; }
 	row& operator [](unsigned i) { return tile[i]; }
 	const row& operator [](unsigned i) const { return tile[i]; }
-	cell& operator ()(unsigned i) { return tile[i / 4][i % 4]; }
-	const cell& operator ()(unsigned i) const { return tile[i / 4][i % 4]; }
+	cell& operator ()(unsigned i) { return tile[i / 3][i % 3]; }
+	const cell& operator ()(unsigned i) const { return tile[i / 3][i % 3]; }
 
 	data info() const { return attr; }
 	data info(data dat) { data old = attr; attr = dat; return old; }
@@ -53,9 +51,10 @@ public:
 	 * return 0 if the action is valid, or -1 if not
 	 */
 	reward place(unsigned pos, cell tile) {
-		if (pos >= 16) return -1;
-		if (tile != 1 && tile != 2) return -1;
+		if (pos >= 6) return -1;
+		if (tile != 1 && tile != 2 && tile != 3) return -1;
 		operator()(pos) = tile;
+        if (tile == 3) return 3;
 		return 0;
 	}
 
@@ -76,100 +75,97 @@ public:
 	reward slide_left() {
 		board prev = *this;
 		reward score = 0;
-		for (int r = 0; r < 4; r++) {
-			auto& row = tile[r];
-			int top = 0, hold = 0;
-			for (int c = 0; c < 4; c++) {
-				int tile = row[c];
-				if (tile == 0) continue;
-				row[c] = 0;
-				if (hold) {
-					if (tile == hold) {
-						row[top++] = ++tile;
-						score += (1 << tile);
-						hold = 0;
-					} else {
-						row[top++] = hold;
-						hold = tile;
-					}
-				} else {
-					hold = tile;
+
+		for (int r = 0; r < 2; r++) {
+			for (int c = 1; c < 3; c++) {
+				if (tile[r][c-1] == 0) {
+					tile[r][c-1] = tile[r][c];
+					tile[r][c] = 0;
+				}
+				else if (tile[r][c] == 1 || tile[r][c] == 2) {
+					if(tile[r][c] + tile[r][c-1] != 3) continue;
+					tile[r][c-1] = 3;
+					score += 3;
+					tile[r][c] = 0;
+				}
+				else if (tile[r][c] == tile[r][c-1]) {
+					tile[r][c-1] += 1;
+					score += pow(3, tile[r][c] - 2);
+					tile[r][c] = 0;
 				}
 			}
-			if (hold) tile[r][top] = hold;
-		}
+        }
+
 		return (*this != prev) ? score : -1;
 	}
+
 	reward slide_right() {
 		reflect_horizontal();
 		reward score = slide_left();
 		reflect_horizontal();
 		return score;
 	}
+
 	reward slide_up() {
-		rotate_right();
-		reward score = slide_right();
-		rotate_left();
-		return score;
-	}
-	reward slide_down() {
-		rotate_right();
-		reward score = slide_left();
-		rotate_left();
-		return score;
+        board prev = *this;
+		reward score = 0;
+
+        for (int c = 0; c < 3; c++) {
+            if (tile[0][c] == 0) {
+                tile[0][c] = tile[1][c];
+                tile[1][c] = 0;
+            }
+            else if (tile[0][c] == 1 || tile[0][c] == 2) {
+                if(tile[0][c] + tile[1][c] != 3) continue;
+                tile[0][c] = 3;
+                score += 3;
+                tile[1][c] = 0;
+            }
+            else if (tile[0][c] == tile[1][c]) {
+                tile[0][c] += 1;
+                score += pow(3, tile[1][c] - 2);
+                tile[1][c] = 0;
+            }
+        }
+
+		return (*this != prev) ? score : -1;
 	}
 
-	void transpose() {
-		for (int r = 0; r < 4; r++) {
-			for (int c = r + 1; c < 4; c++) {
-				std::swap(tile[r][c], tile[c][r]);
-			}
-		}
+	reward slide_down() {
+        reflect_vertical();
+		reward score = slide_up();
+        reflect_vertical();
+		return score;
 	}
 
 	void reflect_horizontal() {
-		for (int r = 0; r < 4; r++) {
-			std::swap(tile[r][0], tile[r][3]);
-			std::swap(tile[r][1], tile[r][2]);
-		}
+        std::swap(tile[0][0], tile[0][2]);
+        std::swap(tile[1][0], tile[1][2]);
 	}
 
 	void reflect_vertical() {
-		for (int c = 0; c < 4; c++) {
-			std::swap(tile[0][c], tile[3][c]);
-			std::swap(tile[1][c], tile[2][c]);
-		}
+        std::swap(tile[0][0], tile[1][0]);
+        std::swap(tile[0][1], tile[1][1]);
+        std::swap(tile[0][2], tile[1][2]);
 	}
-
-	/**
-	 * rotate the board clockwise by given times
-	 */
-	void rotate(int r = 1) {
-		switch (((r % 4) + 4) % 4) {
-		default:
-		case 0: break;
-		case 1: rotate_right(); break;
-		case 2: reverse(); break;
-		case 3: rotate_left(); break;
-		}
-	}
-
-	void rotate_right() { transpose(); reflect_horizontal(); } // clockwise
-	void rotate_left() { transpose(); reflect_vertical(); } // counterclockwise
-	void reverse() { reflect_horizontal(); reflect_vertical(); }
 
 public:
 	friend std::ostream& operator <<(std::ostream& out, const board& b) {
-		for (int i = 0; i < 16; i++) {
-			out << std::setw(std::min(i, 1)) << "" << ((1 << b(i)) & -2u);
+		for (int i = 0; i < 6; i++) {
+            out << std::setw(std::min(i, 1)) << "";
+            if (b(i) > 3)
+			    out << std::pow(2, b(i) - 3) * 3;
+            else
+                out << b(i);
 		}
 		return out;
 	}
 	friend std::istream& operator >>(std::istream& in, board& b) {
-		for (int i = 0; i < 16; i++) {
+		for (int i = 0; i < 6; i++) {
 			while (!std::isdigit(in.peek()) && in.good()) in.ignore(1);
 			in >> b(i);
-			b(i) = std::log2(b(i));
+            if (b(i) > 3)
+			    b(i) = std::log2(b(i) / 3) + 3;
 		}
 		return in;
 	}
