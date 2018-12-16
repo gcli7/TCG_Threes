95e4923c73921215f1e5c46c11e6aa712d1e392c
gcli7 <star106121@gmail.com>
2018-12-17 02:02:01 +0800

    add run.py to the repository

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+N = 3
+i = 0
+#for i in range(0, N):
+while True :
+    #print('Now is', i + 1, ', total is', N)
+    print('Now is', i + 1)
+    i += 1
+    os.system('./Threes --total=1000 --play="load=weights.bin save=weights.bin alpha=0.000075" --save=stat.txt')
+
+    print('sleep')
+    time.sleep(3)
+    print('wake up')
